86070f19c3043fecc3ac4131be973242d955629b
greekmido <onex911@hotmail.com>
2022-05-17 23:24:17 +0200

    adding exercises

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParse = require("body-parser")
 const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://shellparse:Mido1991@cluster0.ogl5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 .then((result)=>{console.log(result.connections[0].name)},(rejected)=>{console.log(rejected)})
-const userSchema = new mongoose.Schema({username:String});
+const userSchema = new mongoose.Schema({username:String,exercise:Array});
 const User = mongoose.model("User",userSchema);
 const exerciseSchema = new mongoose.Schema({username:String,description:String,duration:Number,date:Date,_id:mongoose.Types.ObjectId});
 const Exercise = mongoose.model("Excercise",exerciseSchema);
@@ -32,6 +32,10 @@ app.get("/api/users",(req,res)=>{
   getAllUsers().then((result)=>{res.json(result)})
 })
 
+app.post("/api/users/:_id/exercises",(req,res)=>{
+  User.findById(req.params._id,(err,result)=>{err?console.error(err):Exercise.create({username:result.username,discription:req.body.discription,duration:req.body.duration,date:req.body.date?req.body.date:new Date(),_id:result._id}).then((result)=>console.log(result))});
+  // add exercise field to the user and add the exersise to it as array element
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
